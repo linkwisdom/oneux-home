48e0c571a9ece01570e54bc52213ac229a525c8b
linkwisdom <linkwisdom@qq.com>
2014-08-26 15:10:46 +0800

    修改话术

--- src/main.js
+++ src/main.js
@@ -6,44 +6,31 @@
 define( function (require, exports, module ) {
     var loader = require('common/pageLoader');
     var cindex = 1;
+    var index = 0;
     var pages = $('.page');
     var prePos = document.body.scrollTop;
     var timeStamp = 0;
 
 
-    function notify() {
-
-    }
-
-    function detectScroll(index) {
-        clearTimeout(timer);
-
-        return function () {
-            $(pages.get(index)).scrollintoview({
-                duration: 'normal',
-                complete: function () {
-                    cindex = index;
-                    prePos = document.body.scrollTop;
-                }
-            });
-        }
+    function updataPos() {
+        cindex = index;
+        prePos = document.body.scrollTop;
     }
 
     function bindClick () {
         var vh = document.body.clientHeight;
-
         pages = $('.page');
 
         $(document).scroll(function (e) {
             var ht = document.body.scrollTop;
-            var index = Math.ceil((ht + 50)/ vh) + 1;
             var diff = ht - prePos;
+            prePos = ht;
 
             if (e.timeStamp - timeStamp < 700) {
                 return;
-            } else {
-                timeStamp = e.timeStamp;
             }
+            
+            timeStamp = e.timeStamp;
 
             if ( diff > 30 ) {
                 index = cindex + 1;
@@ -55,20 +42,25 @@ define( function (require, exports, module ) {
 
             $(pages.get(index)).scrollintoview({
                 duration: 500,
-                complete: function () {
-                    cindex = index;
-                    prePos = document.body.scrollTop;
-                }
+                complete: updataPos
             });
         });
 
         $('a').click(function (e) {
-            debugger;
             var it = $(this);
             var link = it.attr('href');
+
+            var ht = document.body.scrollTop;
+            index = Math.ceil((ht + 50)/ vh) + 1;
+
             if (link.charAt(0) == '#') {
                 e.preventDefault();
-                $(link).scrollintoview({ duration: "normal", complete: notify });
+                $(link).scrollintoview(
+                    {
+                        duration: 500,
+                        complete: updataPos 
+                    }
+                );
             }
         });
     }
--- src/page/feature.tpl
+++ src/page/feature.tpl
@@ -4,21 +4,23 @@
         <li class="item">
             <div class="circle"></div>
             <div class="role-name">产品经理</div>
-            <div class="role-feature">还在为毛烦恼？搭建demo从此不跪求RD</div>
+            <div class="role-feature">
+                还在为排期烦恼吗？搭建高保真demo从此不求人。
+            </div>
         </li>
         <li class="item">
             <div class="circle"></div>
-            <div class="role-name">产品经理</div>
-            <div class="role-feature">还在为毛烦恼？搭建demo从此不跪求RD</div>
+            <div class="role-name">设计师</div>
+            <div class="role-feature">提高30%以上的工作效率，专注提升整体体验</div>
         </li>
         <li class="item">
             <div class="circle"></div>
-            <div class="role-name">产品经理</div>
-            <div class="role-feature">还在为毛烦恼？搭建demo从此不跪求RD</div>
+            <div class="role-name">前端工程师</div>
+            <div class="role-feature">提高30%以上的工作效率，专注提升整体体验</div>
         </li>
         <li class="item">
             <div class="circle"></div>
-            <div class="role-name">产品经理</div>
+            <div class="role-name">质量保证</div>
             <div class="role-feature">还在为毛烦恼？搭建demo从此不跪求RD</div>
         </li>
         <li class="item">
